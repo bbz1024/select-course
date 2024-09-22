739dc785ace7d82bef1e681755f5c06cb10ca183
bbz <2632141215@qq.com>
2024-09-22 10:38:13 +0800

    feat: compress

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -6,21 +6,33 @@ if [ -d "output" ]; then
     echo "Cleaning output..."
     rm -rf output
 fi
+# 压缩
+compress=${compress:-0}  # 使用默认值0，如果未定义或为空
 
 mkdir -p /build/output/services
 cd /build/demo7/src || exit
 # build gateway
 
 go build -o /build/output/app
+if [ "$compress" == 1 ]; then
+    echo "compress..."
+    /build/pak/upx -9 /build/output/app
+fi
 echo "build gateway success"
 
 #build service
+
+
 pushd services || exit # enter service dir
 for i in *; do
   name="$i"
   capName="${name^}"
   cd "$i" || exit
   go build -o "/build/output/services/$i/${capName}Service"
+  if [ "$compress" == 1 ]; then
+        echo "compress..."
+        /build/pak/upx -9 "/build/output/services/$i/${capName}Service"
+  fi
   echo "build $i success"
   cd ..
 done
