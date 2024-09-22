b970cc8995e42f2c57774d156bf8e5c88d3e2b54
bbz <2632141215@qq.com>
2024-09-22 10:47:53 +0800

    fixed: label illegal

--- scripts/build-setup.sh
+++ scripts/build-setup.sh
@@ -7,36 +7,25 @@ fi
 
 # start
 echo "starting..."
-
 # 获取当前Git提交的前7位
 GIT_COMMIT_SHORT=$(git rev-parse --short HEAD)
-
 # 获取Jenkins构建号（如果在Jenkins环境中）
 BUILD_NUMBER=${BUILD_NUMBER:-"unknown"}
+IMAGE_NAME=select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT}
+PUSH_IMAGE_NAME=swr.cn-north-4.myhuaweicloud.com/bbz/select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT}
+
 
 # 构建镜像并使用唯一的标签
-docker build -t select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT} .
-
+docker build -t  "${IMAGE_NAME}" .
 echo "build success"
 
 # 推送带有唯一标签的镜像
-docker tag select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT} swr.cn-north-4.myhuaweicloud.com/bbz/select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT}
-docker push swr.cn-north-4.myhuaweicloud.com/bbz/select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT}
-
+docker tag "${IMAGE_NAME}"  "${PUSH_IMAGE_NAME}"
+docker push "${PUSH_IMAGE_NAME}"
 echo "push with unique tag success"
 
-# 如果是最后一个版本，额外添加 latest 标签并推送
-if [ "$BUILD_NUMBER" = "last" ]; then
-  # 重新打上 latest 标签
-  docker tag select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT} swr.cn-north-4.myhuaweicloud.com/bbz/select-course:latest
-  # 推送 latest 标签
-  docker push swr.cn-north-4.myhuaweicloud.com/bbz/select-course:latest
-  echo "push with latest tag success"
-fi
-
 # 清理本地镜像
-docker rmi -f select-course:${BUILD_NUMBER}-${GIT_COMMIT_SHORT}
-
+docker rmi -f "${IMAGE_NAME}"
 echo "clear success"
 
 # k8 replace
